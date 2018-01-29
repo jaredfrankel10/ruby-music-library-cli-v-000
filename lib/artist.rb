5cdc2259b0b2ab0d151d1c84a1476ca054f4b5e3
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 22:32:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,35 +3,35 @@ class Artist
     attr_writer :name
     attr_accessor :songs
     @@all = []
- 
+
     extend Concerns::Findable
- 
+
     def initialize(name)
        @name = name
        @songs = []
     end
- 
+
     def save
        @@all.push(self)
     end
- 
+
     def add_song(song)
        song.artist = self unless song.artist
        songs.push(song) unless songs.include?(song)
     end
- 
+
     def genres
        songs.collect(&:genre).uniq
     end
- 
+
     def self.create(name)
        Artist.new(name).tap(&:save)
     end
- 
+
     def self.all
        @@all
     end
- 
+
     def self.destroy_all
        @@all.clear
     end
