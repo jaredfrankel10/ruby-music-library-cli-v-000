8a7592c04d598ed2a37f311cf7d2b5eb0368e649
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 22:39:27 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,7 +3,7 @@ module Concerns
        def find_by_name(name)
           all.detect { |i| i.name == name }
        end
- 
+
        def find_or_create_by_name(name)
           find_by_name(name) || create(name)
        end
